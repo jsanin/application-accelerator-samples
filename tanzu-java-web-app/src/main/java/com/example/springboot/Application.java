328c3720a249ed2427bfe9247c8b03fd9b2a2e80
Juan Sanin <jsanin@vmware.com>
2023-03-23 13:07:19 -0500

    Add support for http and https

--- tanzu-java-web-app/src/main/java/com/example/springboot/Application.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/Application.java
@@ -2,9 +2,13 @@ package com.example.springboot;
 
 import java.util.Arrays;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
@@ -12,7 +16,9 @@ import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
 public class Application {
-
+	//HTTP port
+	@Value("${http.port}")
+	private int httpPort;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -35,5 +41,19 @@ public class Application {
 	@Bean
 	public HttpTraceRepository htttpTraceRepository() {
 		return new InMemoryHttpTraceRepository();
+	}
+
+	// Let's configure additional connector to enable support for both HTTP and HTTPS
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		return connector;
 	}
 }
